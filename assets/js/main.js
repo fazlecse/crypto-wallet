43cfb10898e51daea3edb2ee2d212604c49294d6
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-04-17 14:27:59 +0600

    17-04-2023
    dash board done

---
+++ assets/js/main.js
@@ -0,0 +1,187 @@
+
+// Preloader area
+var preloader = document.getElementById("preloader");
+function preloder_function() {
+    preloader.style.display = "none";
+}
+
+// add bg to nav
+window.addEventListener("scroll", function () {
+    let scrollpos = window.scrollY;
+    const header = document.querySelector("nav");
+    const headerHeight = header.offsetHeight;
+
+    if (scrollpos >= headerHeight) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
+
+$(document).ready(function () {
+    // owl carousel dashboard card
+    $('.carousel-area1').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 10,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            991: {
+                items: 2
+            }
+        }
+    });
+
+    // What's New carousel
+    $('.carousel-area2').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            575: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            },
+            1400: {
+                items: 5
+            },
+            1620: {
+                items: 6
+            },
+        }
+    });
+    // Stream channel What's New carousel
+    $('.carousel-area3').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            800: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    // responsive-filter
+    $('.carousel-area4').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        // rtl:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            420: {
+                items: 2
+            },
+            550: {
+                items: 3
+            },
+            720: {
+                items: 4
+            },
+        }
+    });
+
+    // Swiper-slider livestream-swiper
+    var swiper = new Swiper(".livestream-swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+
+    });
+
+    // Qr section start
+    var qr = QRCode.generatePNG(document.getElementById("qrUrl").value, {
+        ecclevel: "M",
+        format: "html",
+        margin: 4,
+        modulesize: 8,
+    });
+
+    var img = document.createElement("img");
+    img.src = qr;
+    document.getElementById("qrcode").appendChild(img);
+    var download = (document.getElementById("download-qr").href = qr);
+    // Qr section end
+
+
+
+
+
+
+
+    // select2 filter sidebar
+    $(".js-example-basic-single").select2({
+        maximumSelectionLength: 2
+    });
+    $(".modal-select2").select2({
+        dropdownParent: $("#formModal"),
+    });
+    $(".modal-select3").select2({
+        dropdownParent: $("#formModal2"),
+    });
+
+
+
+});
+
+
+
+// input file preview
+const previewImage = (id) => {
+    document.getElementById(id).src = URL.createObjectURL(event.target.files[0]);
+};
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
